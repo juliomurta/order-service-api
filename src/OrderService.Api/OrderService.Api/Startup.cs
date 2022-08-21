617f7b7b004e415746ef83a331ce093aa25fff48
Júlio César Souza Murta <souzamurtaj@gmail.com>
2022-08-21 19:30:12 -0300

    Change sdk 2.2 to 3.1 (#8)
    * Change sdk 2.2 to 3.1
    
    * Remove azure pipelines
    
    Co-authored-by: Julio Murta <julio.murta2020@hotmail.com>

--- src/OrderService.Api/OrderService.Api/Startup.cs
+++ src/OrderService.Api/OrderService.Api/Startup.cs
@@ -36,7 +36,10 @@ namespace OrderService.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => 
+            {
+                options.EnableEndpointRouting = false;
+            });            
 
             services.AddDbContext<OSContext>(options =>
             {
